fb1c4ed60676f65b65253358cc987f66fabe7f23
barreler126 <129291861+barreler126@users.noreply.github.com>
2024-02-18 16:20:20 +0800

    Update daily_sign.py

--- daily_sign.py
+++ daily_sign.py
@@ -8,7 +8,7 @@ cookie = os.environ.get("JD_COOKIE")
 #        "%22%2C%22rnVersion%22%3A%223.9%22%7D&appid=ld&client=apple&clientVersion=10.0.4&networkType=wifi&osVersion=14"
 #        ".8.1&uuid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&jsonp"
 #        "=jsonp_1645885800574_58482"
-url = r"https://api.m.jd.com/client.action?functionId=signBeanAct&clientVersion=10.0.4&appid=ld&uuid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&openudid=3acd1f6361f86fc0a1bc23971b2e7bbe6197afb6&jsonp=jsonp_1645885800574_58482"
+url = r"https://api.m.jd.com/client.action?functionId=signBeanAct&clientVersion=10.0.4&appid=ld&jsonp=jsonp_1645885800574_58482"
 headers = {"Connection": 'keep-alive',
            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
            "Cache-Control": 'no-cache',
